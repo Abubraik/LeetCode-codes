b27c9c5d065e6e7db40c0c0013638be21ee49a70
Abdullah Abubraik <abdallah2000201@gmail.com>
2022-07-13 12:54:22 +0200

    day 3

---
+++ 206. Reverse Linked List.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        ListNode* temp;
+        while (curr) {
+            temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+};
